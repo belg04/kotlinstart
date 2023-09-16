f873f02e5ca782e15eda097dfee88969469f85b5
belg04 <113061405+belg04@users.noreply.github.com>
2023-09-16 23:32:44 +0200

    ks-1-2 creat new variables and documet cod

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,18 @@
+package lesson_1
+
+fun main()
+{
+    var number =432
+    var name: String = "привет"
+    println(number)
+    println(name)
+
+   // var staff: Short = 2000;
+    var staff: Short = 1999
+    print("Сотрудников в данный момент ")
+    println(staff);
+
+
+
+    //println("Это решение 1 задачи из урока 1")
+}
